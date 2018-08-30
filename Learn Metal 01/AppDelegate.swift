a572cadabb76cc4d726f2864d9e4f8bb915315ab
Maxime CHAPELET <umxprime@gmail.com>
2018-08-30 23:19:55 +0200

    Initial Commit

---
+++ Learn Metal 01/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Learn Metal 01
+//
+//  Created by Maxime CHAPELET on 30/08/2018.
+//  Copyright © 2018 Maxime CHAPELET. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
