a7479440dafcf86a4f6923e17e17d1cca0689501
Maxime CHAPELET <umxprime@gmail.com>
2018-09-02 22:28:23 +0200

    Add target for part 2 and create MTKView subclass

---
+++ Learn Metal 02/MetalView.swift
@@ -0,0 +1,20 @@
+//
+//  MetalView.swift
+//  Learn Metal 02
+//
+//  Created by Maxime CHAPELET on 02/09/2018.
+//  Copyright © 2018 Maxime CHAPELET. All rights reserved.
+//
+
+import Cocoa
+import MetalKit
+
+class MetalView: MTKView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
